3adbf6a3045200e56d2675805d4145281c69763f
Abhinav Kumar <abhinavsiwan@gmail.com>
2016-09-22 20:13:52 -0700

    New files with stopwrods and k-smoothing

---
+++ nbclassify_ksmoothing.py
@@ -0,0 +1,119 @@
+import os
+import sys
+from os import listdir
+from os.path import isfile, join
+import json
+from pprint import pprint
+import math
+import stopwords
+
+
+def classify_enhanced():
+    arg = sys.argv
+    directory = arg[1]
+    # directory = "../Spam or Ham/dev/"
+
+    # read the json data from nbmodel.txt
+    with open("nbmodel.txt") as data_file:
+        data = json.load(data_file)
+
+    # pprint(data["word_dict"])
+    # pprint(data["word_dict"]["Subject:"])
+    # print(data["vocab_size"])
+    # print(data["spam_fcount"])
+    # print(data["ham_fcount"])
+    # print(data["spam_wcount"])
+    # print(data["ham_wcount"])
+
+    # calculate some data to be used in Naive Baye's calculation
+    probab_spam = data["spam_wcount"]/data["vocab_size"]
+    probab_ham = data["ham_wcount"]/data["vocab_size"]
+
+    # used during accuracy and recall calculation
+    ham_file_count = 0
+    spam_file_count = 0
+
+    # Add k smoothing
+    m = 5
+    # No of files classified as ham or spam
+    ham_classified_true = 0
+    spam_classified_true = 0
+    ham_classified_false = 0
+    spam_classified_false = 0
+
+    fout = open("nboutput.txt", "w")
+    for root, subdir, subfiles in os.walk(directory):
+        # print(root)
+        files = [f for f in listdir(root) if isfile(join(root, f))]
+        for file in files:
+            if file.startswith('.'):
+                continue
+            # print(root + "/" + file)
+            f = open(root + "/" + file, "r", encoding="latin1")
+            words = f.read().strip().split()
+            probab_spam_words = 0
+            probab_ham_words = 0
+            for w in words:
+                if w not in stopwords.stopwords:
+                    # Added one smoothing
+                    if w in data["word_dict"]:
+                        # Add k smoothing
+                        probab_ham_words += math.log((data["word_dict"][w]["ham_count"] +
+                                                      (m/data["vocab_size"]))/(data["ham_wcount"] + m))
+                        probab_spam_words += math.log((data["word_dict"][w]["spam_count"] +
+                                                       (m/data["vocab_size"]))/(data["spam_wcount"] + m))
+
+            prob_ham_file = math.log(probab_ham) + probab_ham_words
+            prob_spam_file = math.log(probab_spam) + probab_spam_words
+            if "ham" in root:
+                ham_file_count += 1
+                if prob_ham_file > prob_spam_file:
+                    ham_classified_true += 1
+                    fout.writelines("ham " + root + "/" + file + "\n")
+                else:
+                    ham_classified_false += 1
+                    fout.writelines("spam " + root + "/" + file + "\n")
+            elif "spam" in root:
+                spam_file_count += 1
+                if prob_spam_file > prob_ham_file:
+                    spam_classified_true += 1
+                    fout.writelines("spam " + root + "/" + file + "\n")
+                else:
+                    spam_classified_false += 1
+                    fout.writelines("ham " + root + "/" + file + "\n")
+
+    accuracy_ham = ham_classified_true/ham_file_count
+    accuracy_spam = spam_classified_true/spam_file_count
+
+    # calculating precision for ham and spam
+    precision_spam = spam_classified_true/(spam_classified_true + ham_classified_false)
+    precision_ham = ham_classified_true/(ham_classified_true + spam_classified_false)
+
+    # calculating recall for ham and spam
+    recall_spam = spam_classified_true/spam_file_count
+    recall_ham = ham_classified_true/ham_file_count
+
+    # calculating f1 score for ham and spam
+    f1_spam = (2 * precision_spam * recall_spam)/(precision_spam + recall_spam)
+    f1_ham = (2 * precision_ham * recall_ham)/(precision_ham + recall_ham)
+    '''
+    print("Total ham files:" + str(ham_file_count))
+    print("Total spam files:" + str(spam_file_count))
+    print("Total ham classified true:" + str(ham_classified_true))
+    print("Total ham classified false:" + str(ham_classified_false))
+    print("Total spam classified true:" + str(spam_classified_true))
+    print("Total spam classified false:" + str(spam_classified_false))
+    print()
+    print("ham accuracy:" + str(accuracy_ham))
+    print("spam accuracy:" + str(accuracy_spam))
+    print()
+    print("spam precision:" + str(precision_spam))
+    print("spam recall:" + str(recall_spam))
+    print("spam F1 Score:" + str(f1_spam))
+    print()
+    print("ham precision:" + str(precision_ham))
+    print("ham recall:" + str(recall_ham))
+    print("ham F1 Score:" + str(f1_ham))
+    '''
+
+if __name__ == "__main__":classify_enhanced()
---
+++ nblearn_stopwords.py
@@ -0,0 +1,90 @@
+import sys
+import os
+from os import listdir
+from os.path import isfile, join
+import json
+from pprint import pprint
+import stopwords
+
+# spam_dict = dict()
+# ham_dict = dict()
+word_dict = dict()
+
+
+class Model():
+    # serialize the data and store it in a file nbmodal.txt. This file will be used as input to nbclassify.py
+    def __init__(self, vocab_size, spam_fcount, ham_fcount, spam_wcount, ham_wcount, word_dict=None):
+        self.vocab_size = vocab_size
+        self.spam_fcount = spam_fcount
+        self.ham_fcount = ham_fcount
+        self.spam_wcount = spam_wcount
+        self.ham_wcount = ham_wcount
+        self.word_dict = word_dict
+
+
+def learn():
+    arg = sys.argv
+    directory = arg[1]
+    # directory = "../Spam or Ham/train/"
+
+    wordcount_spam = 0     # No of words in spam
+    wordcount_ham = 0      # No of words in ham
+    spam_file_count = 0    # No of files in spam
+    ham_file_count = 0     # No of file in ham
+
+    # print(directory)
+    punctuation_list = [',', '.', ':', '_', '-', '|', ';']
+    for root, subdir, subfiles in os.walk(directory):
+        # print(root)
+        if "ham" in root:
+            files = [f for f in listdir(root) if isfile(join(root, f))]
+            for file in files:
+                ham_file_count += 1
+                # print(root + "/" + file)
+                f = open(root + "/" + file, "r", encoding="latin1")
+                words = f.read().strip().split()
+                for w in words:
+                    if w not in stopwords.stopwords:
+                        if w in word_dict:
+                            word_dict[w]['ham_count'] += 1
+                        else:
+                            word_dict[w] = {"spam_count": 0, "ham_count": 1}
+                        wordcount_ham += 1
+        elif "spam" in root:
+            files = [f for f in listdir(root) if isfile(join(root, f))]
+            for file in files:
+                spam_file_count += 1
+                # print(root + " " + file)
+                f = open(root + "/" + file, "r", encoding="latin1")
+                words = f.read().strip().split()
+                for w in words:
+                    if w not in stopwords.stopwords:
+                        if w in word_dict:
+                            word_dict[w]['spam_count'] += 1
+                        else:
+                            word_dict[w] = {"spam_count": 1, "ham_count": 0}
+                        wordcount_spam += 1
+
+    vocabulary_size = len(word_dict)
+    '''
+    print("Vocabulary Size:" + str(vocabulary_size))
+    print("spam file count :" + str(spam_file_count))
+    print("ham file count :" + str(ham_file_count))
+    # print("unique words in spam:" + str(len(spam_dict)))
+    # print("unique words in ham:" + str(len(ham_dict)))
+    print("Total Words in Spam:" + str(wordcount_spam))
+    print("Total Words in ham:" + str(wordcount_ham))
+
+    # print(pprint(word_dict))
+    # print(spam_dict.keys())
+    '''
+    # serializing the data
+    obj = Model(vocabulary_size, spam_file_count, ham_file_count, wordcount_spam, wordcount_ham, word_dict)
+    a = json.dumps(vars(obj))
+    fs = open("nbmodel.txt", "w")
+    fs.write(a)
+    fs.close()
+if __name__ == "__main__": learn()
+
+
+# /Users/abhinavkumar/Desktop/USC/Courses_Fall2016/CSCI544_HW/Ass1/Spam\ or\ Ham/train
